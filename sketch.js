093c726912c1fffd3b5bb2075a388f15f9800c2f
Jonathan Craven <jcraven42@gmail.com>
2018-11-09 07:28:28 -0500

    Trying to use the Github API for tracking projects

--- sketch.js
+++ sketch.js
@@ -1,19 +1,43 @@
-var request = new XMLHttpRequest();
-
+let urls;
+let request = new XMLHttpRequest();
+// request.setRequestHeader();
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.github.com', true);
+request.open('GET', 'https://github.com/kravenoff42/MealMaker/projectInfo.json', true);
+
 
 request.onload = function () {
-  // Begin accessing JSON data here
-  // let data = JSON.parse(this.response);
+  console.log(this.response);
+  let list = document.getElementById('list');
+  let data = JSON.parse(this.response);
+  console.log(data);
+  data.forEach(repo => {
+    // let h1 = document.createElement('h1');
+    // h1.innerHTML = repo.name;
+    // list.appendChild(h1);
+    // let url = document.createElement('p')
+    // url.innerHTML = repo.url;
+    // urls.push(repo.url);
+    // list.appendChild(url);
+    // let html_url = document.createElement('p')
+    // html_url.innerHTML = repo.html_url;
+    // list.appendChild(html_url);
+    // let desc = document.createElement('p');
+    // desc.innerHTML = repo.description;
+    // list.appendChild(desc);
+    let info = document.createElement('p');
+    info.innerHTML = repo.status;
+    list.appendChild(desc);
 
-  // data.forEach(repo => {
-  //   // Log each movie's title
-  //   list.appendChild(repo.title);
-  // });
-  let list = document.getElementByID('list');
-  list.innerHTML = JSON.stringify(this.response);
+  });
+  // list.innerHTML = JSON.stringify(this.response);
 }
 
 // Send request
 request.send();
+
+window.onload = ()=>{
+
+}
+
+//raw URL
+// https://raw.githubusercontent.com/kravenoff42/MealMaker/master/projectInfo.json
